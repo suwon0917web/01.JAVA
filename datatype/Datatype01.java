3cb2a87804df144009ff0d7bb203a05f54afe61d
suwon0917web <113746212+suwon0917web@users.noreply.github.com>
2022-09-17 12:17:54 +0900

    Add files via upload

---
+++ datatype/Datatype01.java
@@ -0,0 +1,19 @@
+package datatype;
+
+public class Datatype01 {
+
+	public static void main(String[] args) {
+//		 정수
+//		정수를 저장할수 있는 공간이 필요 --> 만든다
+//		변수를 만드는 방법
+//		자료형 변수명;
+		int age = 10; // 변수 선언과 동시에 초기화
+		int age2 =  age; // 변수 선언과 동시에 초기화
+		int age3;  //변수 선언
+		
+		age3 = 0; // 선언후 초기화		
+		int age4 = age3;
+
+	}
+
+}
