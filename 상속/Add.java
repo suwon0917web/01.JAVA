c912c2dcab920647c423263268a40e0b9e72e8a7
suwon0917web <113746212+suwon0917web@users.noreply.github.com>
2022-10-22 13:44:02 +0900

    Add files via upload

--- 상속/Add.java
+++ 상속/Add.java
@@ -1,10 +1,12 @@
 package »ó¼Ó;
 
+
+
 public class Add extends Calc{
 
 	@Override
 	public Object calculate(Object num1, Object num2) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
 		return (Integer)num1 + (Integer)num2;
 	}
 
