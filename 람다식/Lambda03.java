6d8d1ab424c42110e12e917e2787f24c16dba1c8
suwon0917web <113746212+suwon0917web@users.noreply.github.com>
2022-10-30 12:33:42 +0900

    Add files via upload

--- 람다식/Lambda03.java
+++ 람다식/Lambda03.java
@@ -7,7 +7,30 @@ class Utils{ 	int add(int x, int y) { return x+y; }   }
 
 public class Lambda03 {
 	public static void main(String[] args) {		
-
+		Methmetical m;
+		Pickable p;
+		Computerble c;
+//		m = x-> (x<0)? -x : x ;
+//		System.out.println(m.calculate(100.50));
+//		System.out.println(Math.abs(-100.50));
+		
+//		m = x -> Math.abs(x);
+//		System.out.println(m.calculate(20.5));
+		
+		m = Math::abs;
+		System.out.println(m.calculate(-20.5));
+		
+//		p = (s, i)-> s.charAt(i);
+		String s = "abcdefg";
+//		System.out.println(p.pic(s, 1));
+		
+		p = String::charAt;
+		System.out.println(p.pic(s, 1));
+		
+//		c = (x,y) -> x + y;
+//		Utils u = new Utils(); 
+		c = (new Utils())::add;
+		System.out.println(c.compute(10, 20));
 	}
 
 }
---
+++ 람다식/Lambda04.java
@@ -0,0 +1,42 @@
+package 람다식;
+
+interface NewObject<T>{
+	T getObject(T o);
+}
+interface NewArray<T>{
+	T[] getArray(int size);
+}
+//class NewObjectImple<T>  implements NewObject<T>
+//{
+//	@Override
+//	public T getObject(T o) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}	
+//}
+
+public class Lambda04 {
+
+	public static void main(String[] args) {
+		NewObject<String> s;
+		NewArray<Integer> ia;
+//		s = x -> x;
+//		String str = "123";
+//		System.out.println( s.getObject(str));
+		
+		// 람다를 이용해서 생성자를 호출
+		s = String::new;
+		s.getObject("abc");
+		
+		// 크기가 2인 정수형 배열
+//		Integer[] iary = new Integer[2];
+//		ia = size->new Integer[size];
+		ia = Integer[]::new;
+		Integer[] newAry =  ia.getArray(10);
+		
+		
+		
+		
+	}
+
+}
